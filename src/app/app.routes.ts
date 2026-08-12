ba6e5efb920cd8dfd4673625b5c5cfa962f182ef
K-Micha <Micha.kuehn91@gmail.com>
2026-08-12 18:03:28 +0200

    added routs home and detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./home/home').then((m) => m.Home),
+    },
+    {
+        path: 'survey-detail/:id',
+        loadComponent: () =>
+            import('./survey-detail/survey-detail')
+                .then((m) => m.SurveyDetail),
+    },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { Supabase } from './supabase';
-import { Home } from "./home/home";
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
-  selector: 'app-root',
-  imports: [
-    Home
-],
-  templateUrl: './app.html',
-  styleUrl: './app.scss',
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.scss',
 })
+
 export class App implements OnInit {
   protected readonly title = signal('poll-app');
 
